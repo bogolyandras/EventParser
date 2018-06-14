4a1984427b37c3d76fe9b9062b7f3b58ca1b4ffa
Andras Bogoly <andras.bogoly.91@gmail.com>
2018-06-14 19:30:01 +0200

    Token object tests

---
+++ src/test/java/com/bogolyandras/eventparser/token/TokenTest.java
@@ -0,0 +1,34 @@
+package com.bogolyandras.eventparser.token;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class TokenTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalConstruction1() {
+        new Token<>(null, ExampleEnum.A);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testIllegalConstruction2() {
+        new Token<ExampleEnum>("A", null);
+    }
+
+    @Test
+    public void testIfTheyEquals() {
+        final Token<ExampleEnum> a1 = new Token<>("A", ExampleEnum.A);
+        final Token<ExampleEnum> a2 = new Token<>("A", ExampleEnum.A);
+        final Token<ExampleEnum> b = new Token<>("B", ExampleEnum.B);
+        assertEquals(a1, a2);
+        assertNotEquals(a1, b);
+        final String s = a1.toString();
+        assertTrue(s.startsWith("{"));
+    }
+
+    private enum ExampleEnum { A, B }
+
+}
