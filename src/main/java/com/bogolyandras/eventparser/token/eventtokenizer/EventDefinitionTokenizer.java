5cff709bb09026b511a945afa68eed58bd755f11
Andras Bogoly <andras.bogoly.91@gmail.com>
2018-06-14 19:19:27 +0200

    Tokenizer now based on regex

--- src/main/java/com/bogolyandras/eventparser/token/eventtokenizer/EventDefinitionTokenizer.java
+++ src/main/java/com/bogolyandras/eventparser/token/eventtokenizer/EventDefinitionTokenizer.java
@@ -3,8 +3,6 @@ package com.bogolyandras.eventparser.token.eventtokenizer;
 import com.bogolyandras.eventparser.token.Token;
 import com.bogolyandras.eventparser.token.Tokenizer;
 
-import java.util.Arrays;
-
 public class EventDefinitionTokenizer extends Tokenizer<EventDefinitionTokenKind> {
 
     @Override
@@ -24,15 +22,15 @@ public class EventDefinitionTokenizer extends Tokenizer<EventDefinitionTokenKind
             return EventDefinitionTokenKind.Number;
         } else if (string.matches("([01]?[0-9]|2[0-3]):[0-5][0-9]")) {
             return EventDefinitionTokenKind.HourAndMinute;
-        } else if (Arrays.asList("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday").contains(string)) {
+        } else if (string.matches("(monday|tuesday|wednesday|thursday|friday|saturday|sunday)")) {
             return EventDefinitionTokenKind.Weekday;
-        } else if (string.equals("day")) {
+        } else if (string.matches("(day)")) {
             return EventDefinitionTokenKind.DayWordAfterNumber;
-        } else if (Arrays.asList("week", "month", "year").contains(string)) {
+        } else if (string.matches("(week|month|year)")) {
             return EventDefinitionTokenKind.WeekMonthYear;
-        } else if (string.equals("of")) {
+        } else if (string.matches("(of)")) {
             return EventDefinitionTokenKind.OfPreposition;
-        } else if (string.equals("at")) {
+        } else if (string.matches("(at)")) {
             return EventDefinitionTokenKind.AtPreposition;
         } else {
             throw new IllegalArgumentException("Failed to parse word: '" + string + "'");
